005d58a40ff244d3df006585fe79d364c60c23ed
Dominik Bette <dbette@Dominiks-MacBook-Air.local>
2018-09-21 14:19:36 +0200

    Rename and cleaning

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,10 @@
-unsorted_list = [5, 8, 4, 6, 3, 9, 1, 2, 7]
+cards = [5, 8, 4, 6, 3, 9, 1, 2, 7]
 
-for j in range(1, len(unsorted_list)):
-    key = unsorted_list[j]
-    i = j-1
-    while i>=0 and unsorted_list[i]>key:
-        unsorted_list[i+1] = unsorted_list[i]
+for j in range(1, len(cards)):
+    key = cards[j]
+    i = j - 1
+    while i >= 0 and cards[i]  >key:
+        cards[i+1] = cards[i]
         i -= 1
-    unsorted_list[i+1]=key
-    print(unsorted_list)
+    cards[i+1]=key
+    print(cards)
