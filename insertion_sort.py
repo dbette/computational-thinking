a8d9fb85981def8cbd59da9eb9d2693266d0385a
Dominik Bette <dbette@Dominiks-MacBook-Air.local>
2018-09-21 14:22:26 +0200

    Correction

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,7 +3,7 @@ cards = [5, 8, 4, 6, 3, 9, 1, 2, 7]
 for j in range(1, len(cards)):
     key = cards[j]
     i = j - 1
-    while i >= 0 and cards[i]  >key:
+    while i >= 0 and cards[i] > key:
         cards[i+1] = cards[i]
         i -= 1
     cards[i+1]=key
