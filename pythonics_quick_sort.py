162ad6fbffc675623fa1fae246768a582db83a4f
Dominik Bette <dbette@Dominiks-Air.fritz.box>
2018-09-23 21:19:52 +0200

    Added time complexity analysis

--- pythonics_quick_sort.py
+++ pythonics_quick_sort.py
@@ -1,5 +1,7 @@
 
 import random
+import time
+import matplotlib.pyplot as plt
 
 
 def create_cards(n, method):
@@ -77,3 +79,30 @@ def quicksort(input_array):
 
 cards = create_cards(n=10, method='random')
 print(quicksort(cards))
+
+
+#def iteration(n_iteration, n_cards, method):
+#    measure_time = []
+#    for i in range(0, n_iteration):
+#        cards = create_cards(n_cards, method)
+#        start = time.time()
+#        quicksort(cards)
+#        measure_time.append(time.time()-start)
+#    average_time = sum(measure_time)/len(measure_time)
+#    print("Average time: " + "{0:.9f}".format(average_time) + " seconds")
+#    return(average_time)
+#
+#time_cards = []
+#number_cards = []
+#print("Please wait some time... It's worth it!")
+#for n_cards in range(10, 10000, 100):
+#    time_in_sec = iteration(10, n_cards, method='random')
+#    time_cards.append(time_in_sec)
+#    number_cards.append(n_cards)
+#    
+#    
+#plt.plot(number_cards, time_cards)
+#plt.xlabel("Number cards")
+#plt.ylabel("Average runtime in seconds")
+#plt.title("Quicksort algorithm runtime")
+#plt.show()
